61ab7751872fb8a48ec777ae03ee87090604bd96
mattndross <mattndross@users.noreply.github.com>
2021-08-27 20:14:28 +0200

    add styles

--- scripts/script.js
+++ scripts/script.js
@@ -3,22 +3,25 @@ const rootDiv = document.querySelector("#root");
 
 //create header
 const header = document.createElement('div');
-header.classList.add("header")
-header.innerHTML = "Rick and Morty app";
+header.classList.add("header");
+header.innerHTML = "<h3>Rick and Morty app</h3>";
 rootDiv.appendChild(header);
 
 ///create sidebar and main container
 const rowContainer = document.createElement('div');
-rowContainer.classList.add("row", "100-vh");
+rowContainer.classList.add("row");
 rootDiv.appendChild(rowContainer);
 
 const sidebar = document.createElement('div');
-sidebar.classList.add("sidebar", "col-4", "bg-secondary", "d-flex", "flex-column");
+sidebar.classList.add("sidebar", "col-4", "d-flex", "flex-column");
 rowContainer.appendChild(sidebar);
 
+const superContainer = document.createElement('div');
+superContainer.classList.add("super-container", "col-8");
+rowContainer.appendChild(superContainer);
 const mainContainer = document.createElement('div');
-mainContainer.classList.add("main-container", "col-8", "bg-primary");
-rowContainer.appendChild(mainContainer);
+mainContainer.classList.add("main-container");
+superContainer.appendChild(mainContainer);
 
 const ulOfEpisodes = document.createElement('ul');
 ulOfEpisodes.classList.add("list-group")
@@ -71,7 +74,7 @@ const renderCharacterCard = async (characterUrl) => {
     const response = await fetch(characterUrl);
     const characterObj = await response.json();
     clearDiv(mainContainer);
-    mainContainer.innerHTML = `<div class="row"><img class="col-auto" src="${characterObj.image}" alt="${characterObj.name}"><div class="col-auto"> <h2>${characterObj.name}</h2><p>${characterObj.species} | ${characterObj.status} | ${characterObj.gender} | <span class="location-tag"> ${characterObj.origin.name} </span> </div></div>`;
+    mainContainer.innerHTML = `<div class="row"><img class="col-auto" src="${characterObj.image}" alt="${characterObj.name}"><div class="col-auto"> <h2>${characterObj.name}</h2><p>${characterObj.species} | ${characterObj.status} | ${characterObj.gender} | <strong class="location-tag clickable"> ${characterObj.origin.name} </strong> </div></div>`;
     document.querySelector(".location-tag").onclick = () => {renderLocationCard(characterObj.origin.url)}
     const episodesContainer = document.createElement('div');
     episodesContainer.classList.add("row");
@@ -80,7 +83,7 @@ const renderCharacterCard = async (characterUrl) => {
         const response = await fetch(episodeUrl);
         const espisodeObj = await response.json();
         const container = document.createElement('div');
-        container.classList.add("col-auto")
+        container.classList.add("col-auto", "clickable")
         container.innerHTML = `<h3>Episode ${espisodeObj.id}</h3> <p>${espisodeObj.episode}</p>`;
         container.onclick = () => {renderEpisodeCard(espisodeObj)};
         episodesContainer.appendChild(container)
@@ -94,7 +97,7 @@ const renderMiniCard = async (characterUrl) => {
     const characterObj = await response.json();
     const miniCard = document.createElement('div');
     miniCard.innerHTML = `<img src="${characterObj.image}" alt="${characterObj.name}"><h5>${characterObj.name}</h5><p>${characterObj.species} | ${characterObj.status}</p>`;
-    miniCard.classList.add("col-auto");
+    miniCard.classList.add("col-auto", "clickable");
     miniCard.onclick = () => {renderCharacterCard(characterUrl)};
     charactersContainer.appendChild(miniCard);    
 }
@@ -126,12 +129,13 @@ const getEpisodesList = async (numberOfepisodes) => {
         const url3 = "https://rickandmortyapi.com/api/episode?page=3";
         const response3 = await fetch(url3);
         const parsedRes3 = await response3.json();
-        let arrayOfEpisodes = parsedRes.results;
+        let arrayOfEpisodes = null;
+        arrayOfEpisodes = parsedRes.results;
         arrayOfEpisodes.push(...parsedRes2.results);
         arrayOfEpisodes.push(...parsedRes3.results);
-        renderEpisodesList(arrayOfEpisodes, numberOfepisodes);  
         
-        renderEpisodeCard(arrayOfEpisodes[0])
+        renderEpisodesList(arrayOfEpisodes, numberOfepisodes);        
+        renderEpisodeCard(arrayOfEpisodes[0]);
         
     } catch(error) {
         console.log(error);
@@ -148,7 +152,10 @@ allEpisodesBtn.innerHTML = "SHOW ALL EPISODES";
 sidebar.appendChild(allEpisodesBtn)
 
 document.querySelector("#all-episodes-btn").addEventListener('click', () => {
-    clearDiv(sidebar);
+    clearDiv(ulOfEpisodes);
     getEpisodesList(41)
-})
+});
 
+/* window.onload('load', () => {
+})
+ */
---
+++ tasks.md
@@ -0,0 +1,5 @@
+-fonts, font sizes, color on hover
+margins, paddings, box shadows
+elements size and style
+button's active class
+scrollbar
