c894d6f683979e62d39c647a1a4e88e9ce5feaa0
mattndross <mattndross@users.noreply.github.com>
2021-09-02 23:30:07 +0200

    refactor functions refactor fetch functions refactor render functions in the main-container

--- scripts/script.js
+++ scripts/script.js
@@ -33,28 +33,58 @@ superContainer.appendChild(mainContainer);
 const charactersContainer = document.createElement('div'); // instancio este div fuera de renderEpisode() función para que renderMiniCard() pueda hacer hacer appendChild
 charactersContainer.classList.add("characters-container");
 
+//fetching functions
+let page = 1;
+
+const fetchListOfEpisodes = async () => {
+    try {
+        const url = `https://rickandmortyapi.com/api/episode?page=${page}`;
+        const response = await fetch(url);
+        const parsedResponse = await response.json();
+        return parsedResponse;
+
+    } catch(error) {
+        console.log(error)
+    }
+};
+
+const fetchSingleContent = async (url) => {
+    try {
+        const response = await fetch(url);
+        const parsedResponse = await response.json();
+        return parsedResponse;
+
+    } catch(error) {
+        console.log(error);
+    }
+}
+
+
+
+
 //render functions
 const clearDiv = (div) => {
     div.innerHTML = "";
 }
 
-const renderEpisodeCard = (episode) => {
+const renderEpisodeCard = async (episodeUrl) => {
     clearDiv(mainContainer);
     clearDiv(charactersContainer);
+    const episodeObj = await fetchSingleContent(episodeUrl);
     
     //title (name)
     const episodeTitle = document.createElement('h2');
-    episodeTitle.innerHTML = `Episode ${episode.id} - ${episode.name}`;
+    episodeTitle.innerHTML = `Episode ${episodeObj.id} - ${episodeObj.name}`;
     mainContainer.appendChild(episodeTitle);
     
     //air date and episode code
     const episodeAirDateAndcode = document.createElement('p');
-    episodeAirDateAndcode.innerHTML = `${episode.air_date} | ${episode.episode}`
+    episodeAirDateAndcode.innerHTML = `${episodeObj.air_date} | ${episodeObj.episode}`
     mainContainer.appendChild(episodeAirDateAndcode)
     
     //list of characters
     
-    episode.characters.forEach( characterUrl => {
+    episodeObj.characters.forEach( characterUrl => {
         renderMiniCard(characterUrl);
     });
 
@@ -63,8 +93,7 @@ const renderEpisodeCard = (episode) => {
 
 const renderLocationCard = async (locationUrl) => {
     clearDiv(mainContainer);
-    const response = await fetch(locationUrl);
-    const locationObj = await response.json();
+    const locationObj = await fetchSingleContent(locationUrl);
     mainContainer.innerHTML =`<h2>${locationObj.name}</h2> <p>${locationObj.type} | ${locationObj.dimention}</p>`
     mainContainer.appendChild(charactersContainer);
     locationObj.residents.forEach( resident => {
@@ -73,31 +102,27 @@ const renderLocationCard = async (locationUrl) => {
 
 }
 
-const renderCharacterCard = async (characterUrl) => {
-    const response = await fetch(characterUrl);
-    const characterObj = await response.json();
+const renderCharacterCard = async (characterUrl) => {    
     clearDiv(mainContainer);
+    const characterObj = await fetchSingleContent(characterUrl);
     mainContainer.innerHTML = `<div class="row"><img class="col-auto" src="${characterObj.image}" alt="${characterObj.name}"><div class="col-auto"> <h2>${characterObj.name}</h2><p>${characterObj.species} | ${characterObj.status} | ${characterObj.gender} | <strong class="location-tag clickable"> ${characterObj.origin.name} </strong> </div></div>`;
     document.querySelector(".location-tag").onclick = () => {renderLocationCard(characterObj.origin.url)}
     const episodesContainer = document.createElement('div');
     episodesContainer.classList.add("row");
     mainContainer.appendChild(episodesContainer);
-    characterObj.episode.forEach( async episodeUrl => {
-        const response = await fetch(episodeUrl);
-        const espisodeObj = await response.json();
+    characterObj.episode.forEach( async (episodeUrl) => {
+        const espisodeObj = await fetchSingleContent(episodeUrl);
         const container = document.createElement('div');
         container.classList.add("col-auto", "clickable")
         container.innerHTML = `<h3>Episode ${espisodeObj.id}</h3> <p>${espisodeObj.episode}</p>`;
-        container.onclick = () => {renderEpisodeCard(espisodeObj)};
+        container.onclick = () => {renderEpisodeCard(episodeUrl)};
         episodesContainer.appendChild(container)
 
     })
 }
 
-
 const renderMiniCard = async (characterUrl) => {
-    const response = await fetch(characterUrl);
-    const characterObj = await response.json();
+    const characterObj = await fetchSingleContent(characterUrl);
     const miniCard = document.createElement('div');
     miniCard.innerHTML = `<img src="${characterObj.image}" alt="${characterObj.name}"><h5>${characterObj.name}</h5><p>${characterObj.species} | ${characterObj.status}</p>`;
     miniCard.classList.add("clickable", "mini-card");
@@ -106,22 +131,31 @@ const renderMiniCard = async (characterUrl) => {
 }
 
 //DISPLAY LIST OF ESPISODES ON THE SIDEBAR//
-const renderEpisodesList =  (episodesList, i) => {
-    const trimmedEpisodesList = episodesList.slice(0, i)
-    trimmedEpisodesList.forEach(episode => {
+
+const renderTenEpisodesList =  async (i = 0) => {
+    const fullList = await fetchListOfEpisodes();
+    const trimmedEpisodesList = fullList.slice(i , 10);
+    trimmedEpisodesList.forEach(episodeObj => {
         const liElem = document.createElement('li');
         ulOfEpisodes.appendChild(liElem);
         const btnElem = document.createElement('button');
         btnElem.classList.add("btn", "btn-primary", "list-group-item")
         liElem.appendChild(btnElem);
-        btnElem.innerHTML = `Episode ${episode.id}`
+        btnElem.innerHTML = `Episode ${episodeObj.id}`
         btnElem.onclick = () => {
-            renderEpisodeCard(episode);
+            renderEpisodeCard(episodeObj.url);
         }
     })
 }
 
-const getEpisodesList = async (numberOfepisodes) => {
+
+
+
+
+
+
+
+/* const getEpisodesList = async (numberOfepisodes) => {
     try {
         const url = `https://rickandmortyapi.com/api/episode`;
         const response = await fetch(url);
@@ -138,14 +172,14 @@ const getEpisodesList = async (numberOfepisodes) => {
         arrayOfEpisodes.push(...parsedRes3.results);
         
         renderEpisodesList(arrayOfEpisodes, numberOfepisodes);        
-        renderEpisodeCard(arrayOfEpisodes[0]);
+        renderEpisodeCard(arrayOfEpisodes[0].url);
         
     } catch(error) {
         console.log(error);
     }
 }
 getEpisodesList(10);
-
+ */
 
 //create a button to display all episodes
 const allEpisodesBtn = document.createElement('button');
@@ -159,6 +193,7 @@ document.querySelector("#all-episodes-btn").addEventListener('click', () => {
     getEpisodesList(41)
 });
 
-/* window.onload('load', () => {
-})
- */
+ window.onload = () => {
+    renderEpisodeCard("https://rickandmortyapi.com/api/episode/28")
+};
+ 
